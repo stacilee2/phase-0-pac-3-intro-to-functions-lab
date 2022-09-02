1ddd4afbf08278cd5bde240e9a27e0ae98cc9097
Staci Rosales <stacileep2@gmail.com>
2022-09-02 12:14:14 -0500

    commit tree 25f7d35e354b5d5978004f053d66e00442dedbab

---
+++ index.js
@@ -0,0 +1,18 @@
+string = "Hello";
+
+function shout(string) {
+    return string.toUpperCase();
+  }
+
+const logShout = shout(string);
+console.log(logShout);
+
+function whisper(string) {
+    return string.toLowerCase();
+    console.logWhisper(string);
+  }
+
+const logWhisper = whisper(string);
+console.log(logWhisper);
+
+function sayHiToHeadphonedRoommate ()
